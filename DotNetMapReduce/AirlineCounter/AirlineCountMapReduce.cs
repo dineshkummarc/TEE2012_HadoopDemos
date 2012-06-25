6d5a4fb1aad010bf15fb27dfeb56bbf7c76d5306
matt winkler <mwinkle@microsoft.com>
2012-06-25 13:37:24 -0700

    adding .NET M/R sample

---
+++ DotNetMapReduce/AirlineCounter/AirlineCountMapReduce.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MSDN.Hadoop.MapReduceBase;
+
+namespace AirlineCounter
+{
+    public class AirlineCountMapper : MapperBaseText<Int32>
+    {
+        public override IEnumerable<Tuple<string, int>> Map(string value)
+        {
+            var flightData = value.Split(',');
+            var destinationAirport = flightData[4];
+            var arrivalDelay = Int32.Parse(flightData[0]); 
+            yield return new Tuple<string, int>(destinationAirport,arrivalDelay );
+        }
+    }
+
+    public class AirlineCountReducer : ReducerBase<Int32, String>
+    {
+        public override IEnumerable<Tuple<string, string>> Reduce(string key, IEnumerable<Int32> values)
+        {
+            yield return new Tuple<string, string>(key, values.Count().ToString());
+        }
+    }
+}
