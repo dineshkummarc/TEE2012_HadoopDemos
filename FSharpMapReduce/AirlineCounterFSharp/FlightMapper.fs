6890b6c2548b1d7e824893c7621c59a3194771ac
matt winkler <mwinkle@microsoft.com>
2012-06-26 06:47:39 -0700

    Adding F# demos

---
+++ FSharpMapReduce/AirlineCounterFSharp/FlightMapper.fs
@@ -0,0 +1,17 @@
+﻿namespace AirlineCounterFSharp
+
+open System
+open MSDN.Hadoop.MapReduceBase
+
+type  FlightMapper() =
+    inherit MapperBaseText<Int32>()
+
+    override self.Map (value: string) = 
+        seq {
+            let splits = value.Split(',')
+            let destinationAirport = splits.[4]
+            let arrivalDelay = Int32.Parse(splits.[0]) 
+            yield (destinationAirport, arrivalDelay) 
+        }
+
+
---
+++ FSharpMapReduce/AirlineCounterFSharp/FlightReducer.fs
@@ -0,0 +1,10 @@
+﻿namespace AirlineCounterFSharp
+
+open System
+open MSDN.Hadoop.MapReduceBase
+
+type  FlightReducer() =
+    inherit ReducerBase<Int32, string>()
+
+    override self.Reduce (key:string) (values:seq<Int32>) = 
+        Seq.singleton (key, (values |> Seq.length).ToString())
